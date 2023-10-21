caaa6e1a52484973826a337f9977d65e596f76c5
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2023-10-21 13:49:43 +0700

    add new

---
+++ src/test/java/ForecastWeather/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ForecastWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
